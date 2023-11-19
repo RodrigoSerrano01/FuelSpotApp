32df992d523922a186fa114cc69aac7ccbdff72e
Rodrigo Serrano <rodrigoserrano01@gmail.com>
2023-11-19 15:46:42 -0300

    Clicar no botão do Card do posto cria a rota para o posto

--- app/src/main/java/com/nf/fuelspot/ui/activity/MainActivity.kt
+++ app/src/main/java/com/nf/fuelspot/ui/activity/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.nf.fuelspot.ui.activity
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -15,19 +16,13 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.Source
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
 import com.nf.fuelspot.R
-import com.nf.fuelspot.controller.GasStationController
 import com.nf.fuelspot.controller.MapController
 import com.nf.fuelspot.controller.UserController
 import com.nf.fuelspot.databinding.ActivityMainBinding
 import com.nf.fuelspot.service.GasStationService
 import com.nf.fuelspot.utils.RecyclerViewGasStationUtil
-import java.math.BigDecimal
-import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -43,11 +38,14 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(binding.root)
 
+
+
         val registerButton = findViewById<Button>(R.id.registerButton)
         val logOutButton = findViewById<Button>(R.id.bt_signOut)
         val loginButton = findViewById<Button>(R.id.loginButton)
         val profileButton = findViewById<Button>(R.id.profileButton)
         val textTittle = findViewById<TextView>(R.id.appTittle)
+
 
 
 
