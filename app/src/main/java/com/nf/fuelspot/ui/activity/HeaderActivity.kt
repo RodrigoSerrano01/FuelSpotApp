0c2c5338b9343b1d50b7c1e6cfdc070d5cbc743e
Rodrigo Serrano <rodrigoserrano01@gmail.com>
2023-11-22 16:28:51 -0300

    Arrumei um pouco do codigo.

--- app/src/main/java/com/nf/fuelspot/ui/activity/HeaderActivity.kt
+++ app/src/main/java/com/nf/fuelspot/ui/activity/HeaderActivity.kt
@@ -1,10 +1,13 @@
 package com.nf.fuelspot.ui.activity
 
+import android.content.ContentValues
 import android.content.Context
+import android.util.Log
 import android.widget.Button
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.nf.fuelspot.utils.ButtonActionsUtil
 
   class  HeaderActivity: AppCompatActivity() {
@@ -13,10 +16,10 @@ import com.nf.fuelspot.utils.ButtonActionsUtil
 
       companion object {
 
-
+          private val user = FirebaseAuth.getInstance()
           fun createListener(
               logOutButton: Button,
-              profileButton: Button,
+              profileButton:Button,
               registerButton: Button,
               loginButton: Button,
               textTittle: TextView,
@@ -24,19 +27,25 @@ import com.nf.fuelspot.utils.ButtonActionsUtil
 
               ) {
 
-//              logOutButton.setOnClickListener {
-//
-//                  ButtonActionsUtil.handleLogOutButtonClick(context)
-//
-//
-//              }
-
-              profileButton.setOnClickListener {
-                  ButtonActionsUtil.handleProfileButtonClick(context)
+              logOutButton.setOnClickListener {
+                  Log.d(ContentValues.TAG, "!!Logado não")
+                  user.signOut()
+                  profileButton.setEnabled(false)
+                  logOutButton.setEnabled(false)
+                  profileButton.setVisibility(View.GONE)
+                  logOutButton.setVisibility(View.GONE)
+                  loginButton.setEnabled(true)
+                  registerButton.setEnabled(true)
+                  loginButton.setVisibility(View.VISIBLE)
+                  registerButton.setVisibility(View.VISIBLE)
+                  ButtonActionsUtil.handleComeBackLogoutButton(context)
               }
 
               registerButton.setOnClickListener {
                   ButtonActionsUtil.handleRegisterButtonClick(context)
+              }
+              profileButton.setOnClickListener {
+                  ButtonActionsUtil.handleComeBackProfile(context)
               }
 
               loginButton.setOnClickListener {
--- app/src/main/java/com/nf/fuelspot/ui/activity/ProfileActivity.kt
+++ app/src/main/java/com/nf/fuelspot/ui/activity/ProfileActivity.kt
@@ -21,6 +21,7 @@ class ProfileActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+
     }
 
 
